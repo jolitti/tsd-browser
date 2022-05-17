1820526da3eb5fa56210cf52c2508d748900fdfa
jolitti <marcodeltrionfo@gmail.com>
2022-05-17 09:29:03 +0200

    Reset project to fit the whims of intellij

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+import org.json.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! 2" );
+
+        JSONArray a = new JSONArray("[0,4,5,1]");
+        System.out.println(a);
+    }
+}
