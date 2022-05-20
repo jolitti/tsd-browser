cad0237be1b1ab8da8730479110b066225bc05ab
jolitti <marcodeltrionfo@gmail.com>
2022-05-20 12:41:38 +0200

    Started a mockup of the query system

---
+++ src/main/java/lib/ServiceFilter.java
@@ -0,0 +1,22 @@
+package lib;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+/**
+ Class that represents a particular filter for a service database query.
+
+ For each option, if the value is None it means we aren't filtering based on that parameter
+ */
+public record ServiceFilter (
+    Optional<Set<String>> countries,
+    Optional<Set<String>> providers,
+    Optional<Set<String>> types,
+    Optional<Set<String>> statuses
+    )
+{
+    //private static Set<String> acceptedFilterTypes = Set.of("country","provider","type","status");
+
+}
