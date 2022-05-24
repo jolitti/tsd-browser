56765cea8fbb3593193e8c71d2096532c68e5eb1
DaMole98 <damiano.salvaterra@studenti.unipd.it>
2022-05-24 20:15:34 +0200

    spostata la funzione matches

--- src/main/java/lib/ServiceFilter.java
+++ src/main/java/lib/ServiceFilter.java
@@ -19,4 +19,30 @@ public record ServiceFilter (
 {
     //private static Set<String> acceptedFilterTypes = Set.of("country","provider","type","status");
 
+    public boolean matches(Service service){
+
+        boolean matchesCountry = true;
+        boolean matchesTSP = true;
+        boolean matchesType = true;
+        boolean matchesStatus = true;
+
+        if(this.countries().isPresent())
+            matchesCountry =
+                    this.countries().get().contains(service.countryCode());
+
+        if(this.providers().isPresent())
+            matchesTSP =
+                    this.providers().get().contains(service.tspId());
+
+        if(this.types().isPresent())
+            matchesType =
+                    this.types().get().contains(service.type());
+
+        if(this.statuses().isPresent())
+            matchesStatus =
+                    this.types().get().contains(service.currentStatus());
+
+        return matchesCountry && matchesTSP && matchesType && matchesStatus;
+    }
 }
+
