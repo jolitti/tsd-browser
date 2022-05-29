001987eb03b0d486fd7bd5e75b69171e32bccf88
Alessandro Barel <>
2022-05-29 09:52:58 +0200

    .

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module it.unipd.sweng {
+    requires javafx.controls;
+    requires javafx.fxml;
+    //requires javafx.web;
+
+    requires org.controlsfx.controls;
+    //requires com.dlsc.formsfx;
+    //requires validatorfx;
+    //requires org.kordamp.ikonli.javafx;
+    //requires org.kordamp.bootstrapfx.core;
+    //requires eu.hansolo.tilesfx;
+    //requires com.almasb.fxgl.all;
+    requires org.json;
+
+    opens it.unipd.sweng to javafx.fxml;
+    exports it.unipd.sweng;
+}
