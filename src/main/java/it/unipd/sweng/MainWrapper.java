042706a35b00db24bbba6f3bde8d9dc531c7419b
jolitti <marcodeltrionfo@gmail.com>
2022-07-04 21:33:57 +0200

    Created MainWrapper, added "out/" to gitignore

---
+++ src/main/java/it/unipd/sweng/MainWrapper.java
@@ -0,0 +1,8 @@
+package it.unipd.sweng;
+
+// This class exists for compatibility with maven.shade
+public class MainWrapper {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
